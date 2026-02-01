52e9087ad186fe1184481ab57220caeb8f4ace7e
x1545x-rgb <x1545x@gmail.com>
2026-02-01 17:04:01 +0900

    Update 404bg.js

--- 404bg.js
+++ 404bg.js
@@ -197,7 +197,7 @@ async function loadObjFromUrl(url, objectScale = 1.0) {
 }
 
 // OBJ ファイルのパスは実際のサイト構成に合わせて調整してください
-loadObjFromUrl('/models/object_404.obj', 0.5).catch(err => {
+loadObjFromUrl('https://cdn.jsdelivr.net/gh/x1545x-rgb/findex-assets@v1.3.2/models/object_404.obj', 0.5).catch(err => {
   console.warn('404bg.js: OBJ load failed:', err.message);
 });
 
@@ -377,3 +377,4 @@ function animate() {
 }
 
 animate();
+
